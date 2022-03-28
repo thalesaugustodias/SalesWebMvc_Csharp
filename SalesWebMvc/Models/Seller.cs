ca9328d0ffa6e5cddf28b1fd9b25a3b27002d432
Thales Dias <thalesdias97@gmail.com>
2022-03-28 10:50:57 -0300

    Seeding Service

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Models
 
         }
 
-        public Seller(int id, string name, DateTime birthDate, double baseSalary, Department department)
+        public Seller(int id, string name, string v, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
